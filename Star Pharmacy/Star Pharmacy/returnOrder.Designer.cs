d0921de2b2be5003d064e6ffd7438da532877ac1
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2017-07-12 21:45:06 +0530

    Return customer items interface

--- Star Pharmacy/Star Pharmacy/returnOrder.Designer.cs
+++ Star Pharmacy/Star Pharmacy/returnOrder.Designer.cs
@@ -29,51 +29,152 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtBoxCreditNoteNo = new System.Windows.Forms.TextBox();
+            this.lblCreditNote = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.InvoiceNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtBoxCreditNoteNo);
+            this.groupBox1.Controls.Add(this.lblCreditNote);
             this.groupBox1.Controls.Add(this.dataGridView1);
             this.groupBox1.Location = new System.Drawing.Point(57, 47);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(662, 231);
+            this.groupBox1.Size = new System.Drawing.Size(662, 243);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Return Item";
             // 
-            // groupBox2
+            // txtBoxCreditNoteNo
             // 
-            this.groupBox2.Location = new System.Drawing.Point(57, 314);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(662, 156);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Return Items";
+            this.txtBoxCreditNoteNo.Location = new System.Drawing.Point(105, 30);
+            this.txtBoxCreditNoteNo.Name = "txtBoxCreditNoteNo";
+            this.txtBoxCreditNoteNo.ReadOnly = true;
+            this.txtBoxCreditNoteNo.Size = new System.Drawing.Size(148, 20);
+            this.txtBoxCreditNoteNo.TabIndex = 2;
+            this.txtBoxCreditNoteNo.TextChanged += new System.EventHandler(this.txtBoxCreditNoteNo_TextChanged);
+            // 
+            // lblCreditNote
+            // 
+            this.lblCreditNote.AutoSize = true;
+            this.lblCreditNote.Location = new System.Drawing.Point(16, 33);
+            this.lblCreditNote.Name = "lblCreditNote";
+            this.lblCreditNote.Size = new System.Drawing.Size(83, 13);
+            this.lblCreditNote.TabIndex = 1;
+            this.lblCreditNote.Text = "Credit Note No :";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(18, 45);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.InvoiceNo});
+            this.dataGridView1.Location = new System.Drawing.Point(19, 71);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(624, 146);
+            this.dataGridView1.Size = new System.Drawing.Size(615, 90);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // InvoiceNo
+            // 
+            this.InvoiceNo.HeaderText = "Invoice No.";
+            this.InvoiceNo.Name = "InvoiceNo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRemove);
+            this.groupBox2.Controls.Add(this.btnCancel);
+            this.groupBox2.Controls.Add(this.btnReturn);
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Location = new System.Drawing.Point(57, 315);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(662, 233);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Return Items";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(559, 193);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(442, 193);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(19, 42);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(615, 115);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(523, 184);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(111, 23);
+            this.btnReturn.TabIndex = 1;
+            this.btnReturn.Text = "Create Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(381, 184);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel Return";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(19, 184);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(113, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove Item";
+            this.btnRemove.UseVisualStyleBackColor = true;
             // 
             // returnOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(821, 482);
+            this.ClientSize = new System.Drawing.Size(814, 560);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "returnOrder";
             this.Text = "returnOrder";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +184,14 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtBoxCreditNoteNo;
+        private System.Windows.Forms.Label lblCreditNote;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InvoiceNo;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnRemove;
     }
 }
--- Star Pharmacy/Star Pharmacy/returnOrder.cs
+++ Star Pharmacy/Star Pharmacy/returnOrder.cs
@@ -12,9 +12,28 @@ namespace Star_Pharmacy
 {
     public partial class returnOrder : Form
     {
+        private static int CreditNoteNo = 0;
+
         public returnOrder()
         {
+            CreditNoteNo++;
             InitializeComponent();
+            setTxtCreditNoteNo();
+        }
+
+        private void txtBoxCreditNoteNo_TextChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("You can't change Current Credit Note Value!");
+        }
+
+        private void setTxtCreditNoteNo()
+        {
+            txtBoxCreditNoteNo.Text = CreditNoteNo.ToString();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
