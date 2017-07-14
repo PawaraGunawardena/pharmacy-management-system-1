735b5b2174cabf8605c7ee3bdf112c19b0b39c79
dasunpubudumal <Birthday@19940730>
2017-07-14 20:11:35 +0530

    Download the PDFSharp Library and Add the Reference like stated here.
    http://csharp.net-informations.com/file/create-pdf.htm

--- Star Pharmacy/Star Pharmacy/Sales_History.Designer.cs
+++ Star Pharmacy/Star Pharmacy/Sales_History.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.invoice_chkbox = new System.Windows.Forms.CheckBox();
             this.branch_chkbox = new System.Windows.Forms.CheckBox();
             this.date_chkbox = new System.Windows.Forms.CheckBox();
@@ -43,7 +44,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.from_dtp = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
@@ -62,6 +63,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.invoice_chkbox);
             this.groupBox1.Controls.Add(this.branch_chkbox);
@@ -82,6 +84,16 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Show by";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(533, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 36);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Generate A Report HTML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // invoice_chkbox
             // 
@@ -219,15 +231,15 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "From";
             // 
-            // button2
+            // button3
             // 
-            this.button2.Location = new System.Drawing.Point(533, 76);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(117, 36);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Generate A Report";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button3.Location = new System.Drawing.Point(310, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 36);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Generate A Report and Mail";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Sales_History
             // 
@@ -268,5 +280,6 @@
         private System.Windows.Forms.CheckBox branch_chkbox;
         private System.Windows.Forms.CheckBox date_chkbox;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
